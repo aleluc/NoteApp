6fbcde4d23997d39b96c9744d223b83b158083b0
aleksander.luczak <aleksander.luczak@gmail.com>
2021-07-31 12:21:14 +0200

    Added Note class

--- noteapp/notes/models.py
+++ noteapp/notes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models import fields
 
-# Create your models here.
+
+class Note(models.Model):
+    title = fields.CharField(max_length=20)
+    content = fields.TextField
+    creation_date = fields.TimeField(auto_now_add=True)
