682c4dd72f637cb9b298e3dd2c8f045243b96a13
aleksander.luczak <aleksander.luczak@gmail.com>
2021-07-31 17:20:42 +0200

    Added basic functional test

--- noteapp/notes/tests.py
+++ noteapp/notes/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from selenium import webdriver
 
-# Create your tests here.
+class FunctionalTestCase(TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def test_homepage_exists(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('home view', self.browser.page_source)
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+
+class UnitTestCase(TestCase):
+    pass
