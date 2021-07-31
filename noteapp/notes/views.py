395bb1415f20d6ae4feafa20a24fb6db6b3b40b7
aleksander.luczak <aleksander.luczak@gmail.com>
2021-07-31 21:50:28 +0200

    Added note form to the home view

--- noteapp/notes/views.py
+++ noteapp/notes/views.py
@@ -2,11 +2,14 @@ from django.http.response import Http404, HttpResponse
 from django.shortcuts import render
 
 from .models import Note
+from .forms import AddForm
 
 def home(request):
+    form = AddForm()
     notes = Note.objects.all()
     return render(request, 'home.html', {
         'notes': notes,
+        'form': form,
     })
 
 def note_details(request, note_id):
