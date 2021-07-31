4a822760135b8ba09e54dc6506f514f1736b92f9
aleksander.luczak <aleksander.luczak@gmail.com>
2021-07-31 21:11:19 +0200

    Add more test stubs

--- noteapp/notes/tests.py
+++ noteapp/notes/tests.py
@@ -10,6 +10,18 @@ class FunctionalTestCase(TestCase):
         self.browser.get('http://localhost:8000')
         self.assertIn('All notes', self.browser.page_source)
 
+    def test_add_note(self):
+        pass
+
+    def test_remove_note(self):
+        pass
+
+    def test_edit_note(self):
+        pass
+
+    def test_share_note(self):
+        pass
+
     def tearDown(self) -> None:
         self.browser.quit()
         return super().tearDown()
