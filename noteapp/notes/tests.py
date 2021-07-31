805eb30784da0c38f09ad801c1883747f44cca61
aleksander.luczak <aleksander.luczak@gmail.com>
2021-07-31 21:49:27 +0200

    Add more tests

--- noteapp/notes/tests.py
+++ noteapp/notes/tests.py
@@ -1,30 +1,58 @@
 from django.test import TestCase
 from selenium import webdriver
 
+from .forms import AddForm
+
 class FunctionalTestCase(TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
 
     def test_homepage_exists(self):
+        # when
         self.browser.get('http://localhost:8000')
+
+        # then
         self.assertIn('All notes', self.browser.page_source)
 
     def test_add_note(self):
-        pass
+        # given
+        self.browser.get('http://localhost:8000')
 
-    def test_remove_note(self):
-        pass
+        # when
+        self.browser.find_element_by_name('Add Note').click()
+        title = self.browser.find_element_by_id('id_title')
+        title.send_keys('Test note')
+        content = self.browser.find_element_by_id('id_content')
+        content.send_keys('Test content')
+        self.browser.find_element_by_name('submit').click()
 
-    def test_edit_note(self):
-        pass
+        # then
+        self.browser.find_element_by_name('Test note').click()
+        self.assertIn('Test content', self.browser.page_source)
 
-    def test_share_note(self):
-        pass
+    # def test_remove_note(self):
+    #     pass
+
+    # def test_edit_note(self):
+    #     pass
+
+    # def test_share_note(self):
+    #     pass
 
     def tearDown(self) -> None:
         self.browser.quit()
         return super().tearDown()
 
 class UnitTestCase(TestCase):
-    pass
+
+    def test_home_page_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_add_form(self):
+        form = AddForm(data={
+            'title': 'Test note',
+            'content': 'Test content',
+        })
+        self.assertTrue(form.is_valid())
