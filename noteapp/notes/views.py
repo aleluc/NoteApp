0c079dbd0f665f040e3fa2e004c9b03b1cf3e870
aleksander.luczak <aleksander.luczak@gmail.com>
2021-07-31 22:36:13 +0200

    Added removing note feature

--- noteapp/notes/views.py
+++ noteapp/notes/views.py
@@ -1,5 +1,5 @@
 from django.http.response import Http404, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .models import Note
 from .forms import AddNoteForm
@@ -25,6 +25,18 @@ def home(request):
 def note_details(request, note_id):
     try:
         note = Note.objects.get(id=note_id)
+    except Note.DoesNotExist:
+        raise Http404('Note does not exist')
+    return render(request, 'note_details.html', {
+        'note': note,
+    })
+
+def note_remove(request, note_id):
+    try:
+        note = Note.objects.get(id=note_id)
+        if request.method == "POST":
+            note.delete()
+            return redirect('/')
     except Note.DoesNotExist:
         raise Http404('Note does not exist')
     return render(request, 'note_details.html', {
