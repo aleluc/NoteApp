502c903c6b5e83e4797fa0e35dd27d2422f2de12
aleksander.luczak <aleksander.luczak@gmail.com>
2021-08-03 21:43:25 +0200

    Added owner and shared_with fields

--- noteapp/notes/models.py
+++ noteapp/notes/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.db.models import fields
+from django.contrib.auth.models import User
 
 
 class Note(models.Model):
     title = fields.CharField(max_length=20)
     content = fields.TextField(default="")
     creation_date = fields.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User)
+    shared_with = models.ManyToManyField(User)
+
+    def share_note(self):
+        pass
