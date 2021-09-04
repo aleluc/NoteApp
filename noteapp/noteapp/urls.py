d43b93c60d7b3261baafd9e82e4b47e5221053e6
aleksander.luczak <aleksander.luczak@gmail.com>
2021-09-04 12:22:54 +0200

    Added all auth views

--- noteapp/noteapp/urls.py
+++ noteapp/noteapp/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.urls.conf import include
 
 from notes import views
 
@@ -11,5 +12,5 @@ urlpatterns = [
     path('add/', views.add_note, name='add_note'),
     path('remove/<int:note_id>/', views.remove_note, name='remove_note'),
     path('edit/<int:note_id>/', views.edit_note, name='edit_note'),
-    path('accounts/login/', auth_views.LoginView.as_view()),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
